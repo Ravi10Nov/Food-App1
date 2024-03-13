3cc07b799f8f25660d8794049076253eaf6ed567
Ravi Kant <ravikantas.kant81@gmail.com>
2024-03-13 17:53:27 +0530

    remove google api key

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -2,7 +2,6 @@ import { GoogleMap,useLoadScript,MarkerF } from "@react-google-maps/api";
 
 const MapComponent = () =>{
     const {isLoaded , loadError} = useLoadScript({
-        // googleMapsApiKey:AIzaSyBw6BZ12M4-50w--evL_VPFkNgGnM-NM94,
     })
 }
 
